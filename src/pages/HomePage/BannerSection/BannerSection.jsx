5f768136e698e52076f05444b87b2b1f74b1247d
shazzed-76 <shazzed7656@gmail.com>
2025-07-29 18:46:01 +0600

    completed banner section as requirements

--- src/pages/HomePage/BannerSection/BannerSection.jsx
+++ src/pages/HomePage/BannerSection/BannerSection.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BiSolidBadgeCheck } from "react-icons/bi";
 import bloodImage from "../../../assets/images/blood-drop.png";
+import { useNavigate } from 'react-router';
 
 
 const BannerSection = () => {
+    const navigate = useNavigate();
     return (
       <section className=" bg-gradient-to-br from-[#ffffff] to-[#f0f4ff] dark:from-[#1d232a] dark:to-[##1a1f26]">
         <div className="flex flex-col-reverse md:flex-row py-10 gap-10 items-center justify-between max-w-7xl mx-auto px-5 md:px-10 h-auto md:h-[calc(100vh-89px)]">
@@ -16,7 +18,20 @@ const BannerSection = () => {
               the reason <br /> someone lives another day.
             </p>
 
-            <button className="btn btn-outline btn-primary">Get Started</button>
+            <div className="flex gap-5 items-center">
+              <button
+                onClick={() => navigate("/register")}
+                className="btn  btn-primary"
+              >
+                Join as a donor
+              </button>
+              <button
+                onClick={() => navigate("/search")}
+                className="btn btn-outline btn-primary"
+              >
+                Search Donors
+              </button>
+            </div>
           </div>
 
           <div className="max-w-sm w-full relative z-1  overflow-hidden">
