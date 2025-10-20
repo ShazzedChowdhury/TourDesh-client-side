1c345ee290f787e529c149ed242a956ca9d1b0dd
shazzed-76 <shazzed7656@gmail.com>
2025-10-20 06:16:20 +0600

    update hero section

--- src/pages/HomePage/BannerSection/BannerSection.jsx
+++ src/pages/HomePage/BannerSection/BannerSection.jsx
@@ -1,53 +1,50 @@
-import React from 'react';
+import React from "react";
 import { BiSolidBadgeCheck } from "react-icons/bi";
-import bannerImg from "../../../assets/images/Banner-img.png"
-import { useNavigate } from 'react-router';
-import useRole from '../../../hooks/useRole';
-
+import bannerImg from "../../../assets/images/Banner-img.png";
+import { useNavigate } from "react-router";
+import useRole from "../../../hooks/useRole";
 
 const BannerSection = () => {
-    const navigate = useNavigate();
-    return (
-      <section className=" bg-gradient-to-br from-[#ffffff] to-[#f0f4ff] dark:from-[#1d232a] dark:to-[##1a1f26]">
-        <div className="flex flex-col-reverse md:flex-row py-10 gap-10 items-center justify-between max-w-7xl mx-auto px-5 md:px-10 h-auto md:h-[500px]">
-          <div className="space-y-5">
-            <h1 className="text-3xl md:text-5xl font-bold">
-              Discover <br />
-              the <span className="text-[#3fd0d3]">World</span> with <br />{" "}
-              Trusted Tour Guides
-            </h1>
-            <p className="text-lg font-semibold text-gray-400">
-              Turn every trip into an unforgettable adventure. Explore hidden
-              gems, connect with local cultures, and enjoy stress-free travel
-              with our experienced tour guides by your side
-            </p>
+  const navigate = useNavigate();
+  return (
+    <div
+      className="hero h-[700px]"
+      style={{
+        backgroundImage: "url(https://i.ibb.co/bggrrLq4/tour-Desh-banner.jpg)",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content">
+        <div className="space-y-5">
+          <h1 className="text-3xl md:text-6xl font-bold">
+            Discover <br />
+            the <span className="text-[#3fd0d3]">World</span> with <br />{" "}
+            Trusted Tour Guides
+          </h1>
+          <p className="text-lg font-semibold text-white lg:w-3/5">
+            Turn every trip into an unforgettable adventure. Explore hidden
+            gems, connect with local cultures, and enjoy stress-free travel with
+            our experienced tour guides by your side
+          </p>
 
-            <div className="flex gap-5 items-center">
-                <button
-                  onClick={() => navigate("/dashboard/join-as-tour-guide")}
-                  className="btn  btn-primary"
-                >
-                  Join as tour guide
-                </button>
-              <button
-                onClick={() => navigate("/all-trips")}
-                className="btn btn-outline btn-primary"
-              >
-                All packages
-              </button>
-            </div>
-          </div>
-
-          <div className="max-w-sm w-full relative z-1  overflow-hidden">
-            <img
-              src={bannerImg}
-              alt="banner image"
-              className="object-cover object-top w-full"
-            />
+          <div className="flex gap-5 items-center">
+            <button
+              onClick={() => navigate("/dashboard/join-as-tour-guide")}
+              className="btn  btn-primary"
+            >
+              Join as tour guide
+            </button>
+            <button
+              onClick={() => navigate("/all-trips")}
+              className="btn btn-outline btn-primary"
+            >
+              All packages
+            </button>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BannerSection;
